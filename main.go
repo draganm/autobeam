a94d9fc4c41030c749914353c8d57c5fc691ce9a
Dragan Milic <dragan@netice9.com>
2024-10-31 11:43:42 +0100

    getting tags, supporting subdirs

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/urfave/cli/v2"
 )
 
@@ -11,7 +14,18 @@ func main() {
 	app := &cli.App{
 		Name: "autobeam",
 		Action: func(c *cli.Context) error {
-			repo, err := git.PlainOpen(".")
+
+			dir, err := filepath.Abs(".")
+			if err != nil {
+				return fmt.Errorf("failed to get absolute path of the current dir: %w", err)
+			}
+
+			repoRoot, err := findRepositoryRoot(dir)
+			if err != nil {
+				return fmt.Errorf("failed to find repository root: %w", err)
+			}
+
+			repo, err := git.PlainOpen(repoRoot)
 			if err != nil {
 				return fmt.Errorf("failed to open git repo: %w", err)
 			}
@@ -32,10 +46,39 @@ func main() {
 			if err != nil {
 				return fmt.Errorf("failed to get status: %w", err)
 			}
+
+			tags, err := repo.Tags()
+			if err != nil {
+				return fmt.Errorf("failed to get tags: %w", err)
+			}
+
+			fmt.Println("tags:")
+			err = tags.ForEach(func(r *plumbing.Reference) error {
+				fmt.Println(" ", r.Name().Short())
+				return nil
+			})
+			if err != nil {
+				return fmt.Errorf("failed to iterate tags: %w", err)
+			}
+
 			fmt.Println("is clean", status.IsClean())
 			return nil
 
 		},
 	}
 	app.RunAndExitOnError()
+}
+
+func findRepositoryRoot(dir string) (string, error) {
+	for {
+		if _, err := os.Stat(filepath.Join(dir, ".git")); err == nil {
+			return dir, nil
+		}
+
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			return "", fmt.Errorf("no git repository found")
+		}
+		dir = parent
+	}
 }
